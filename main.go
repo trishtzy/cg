0038a6acdc4f76ba65166a128185376bf2d2a7e6
Tricia Tan Zi Ying <tricia.tzy@tutanota.com>
2021-03-14 00:21:03 +0800

    Index countries

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
 
 	elasticsearch "github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/trishtzy/cg/model"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -15,7 +21,8 @@ func main() {
 	// Enable line numbers in logging
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	var (
-		r map[string]interface{}
+		r  map[string]interface{}
+		wg sync.WaitGroup
 	)
 
 	// Connect to DB and migrate model
@@ -31,11 +38,6 @@ func main() {
 		}
 	}
 
-	// cfg := elasticsearch.Config{
-	// 	Addresses: []string{
-	// 		"http://localhost:9200",
-	// 	},
-	// }
 	es, _ := elasticsearch.NewDefaultClient()
 	if err != nil {
 		log.Fatalf("Error creating the client: %s", err)
@@ -58,4 +60,44 @@ func main() {
 	log.Printf("Server: %s", r["version"].(map[string]interface{})["number"])
 	log.Println(strings.Repeat("~", 37))
 
+	var countries []model.Country
+	result := db.Find(&countries)
+	if result.Error != nil {
+		log.Println(result.Error.Error())
+	}
+
+	for i, country := range countries {
+		wg.Add(1)
+		go func(i int, country model.Country) {
+			defer wg.Done()
+
+			b, err := json.Marshal(country)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			req := esapi.IndexRequest{
+				Index:        "testa",
+				DocumentType: "search_as_you_type",
+				DocumentID:   strconv.FormatUint(uint64(country.ID), 10),
+				Body:         strings.NewReader(string(b)),
+				Refresh:      "true",
+			}
+
+			res, err := req.Do(context.Background(), es)
+			if err != nil {
+				log.Fatalf("Error getting response while indexing: %s", err)
+			}
+			defer res.Body.Close()
+
+			if res.IsError() {
+				log.Printf("[%s] Error indexing document ID=%d", res.Status(), country.ID)
+			} else {
+				if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+					log.Printf("Error parsing indexRequest response body: %s", err)
+				}
+				log.Printf("%v", r)
+			}
+		}(i, country)
+	}
+	wg.Wait()
 }
